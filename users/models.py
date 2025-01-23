55a4b35704606c8fb16059033c856b9bec49c07b
Theo Lee <theolee021@gmail.com>
2025-01-22 21:08:53 -0800

    Update Users Model

--- users/models.py
+++ users/models.py
@@ -2,4 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    first_name = models.CharField(max_length=150, editable=False, blank=True)
+    last_name = models.CharField(max_length=150, editable=False, blank=True)
+    name = models.CharField(max_length=150, blank=True)
+    is_host = models.BooleanField(default=False)
